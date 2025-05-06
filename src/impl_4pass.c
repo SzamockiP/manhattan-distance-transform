b92ef87cc1c2d9520ea7da87c74e78009618c7d4
SzamockiP <szamocki.piotr04@gmail.com>
2025-05-06 10:41:05 +0200

    removed multiplication from 2 pass impl

--- src/impl_4pass.c
+++ src/impl_4pass.c
@@ -3,15 +3,15 @@
 
 void manhattan_distance_transform(int* input_array, int* output_array){
 
-    int i, j, idx, is_neighbour, row_idx, neighbour_idx;
+    int i, j, idx, is_neighbour, row_offset, neighbour_idx;
 
     const int INF = ARRAY_SIZE * ARRAY_SIZE;
 
     // Iteration 1: initialization – neighbour marking
-    row_idx = 0;
+    row_offset = 0;
     for(i = 0; i < ARRAY_SIZE; i++) {
         for(j = 0; j < ARRAY_SIZE; j++) {
-            idx = row_idx + j;
+            idx = row_offset + j;
             is_neighbour = 0;
             // Top check
             if(i > 0 && input_array[idx - ARRAY_SIZE] != input_array[idx])
@@ -28,54 +28,54 @@ void manhattan_distance_transform(int* input_array, int* output_array){
 
             output_array[idx] = is_neighbour ? 0 : INF;
         }
-        row_idx += ARRAY_SIZE;
+        row_offset += ARRAY_SIZE;
     }
 
     // Iteration 2: Top to bottom
-    row_idx = ARRAY_SIZE;
+    row_offset = ARRAY_SIZE;
     for(i = 1; i < ARRAY_SIZE; i++) {
         for(j = 0; j < ARRAY_SIZE; j++) {
-            idx = row_idx + j;
+            idx = row_offset + j;
             neighbour_idx = idx - ARRAY_SIZE;
             if(output_array[neighbour_idx] + 1 < output_array[idx])
                 output_array[idx] = output_array[neighbour_idx] + 1;
         }
-        row_idx += ARRAY_SIZE;
+        row_offset += ARRAY_SIZE;
     }
 
     // Iteration 3: Left to right
-    row_idx = 0;
+    row_offset = 0;
     for(i = 0; i < ARRAY_SIZE; i++) {
         for(j = 1; j < ARRAY_SIZE; j++) {
-            idx = row_idx + j;
+            idx = row_offset + j;
             neighbour_idx = idx - 1;
             if(output_array[neighbour_idx] + 1 < output_array[idx])
                 output_array[idx] = output_array[neighbour_idx] + 1;
         }
-        row_idx += ARRAY_SIZE;
+        row_offset += ARRAY_SIZE;
     }
 
     // Iteration 4: Bottom to top
-    row_idx = (ARRAY_SIZE - 2) * ARRAY_SIZE;
+    row_offset = (ARRAY_SIZE - 2) * ARRAY_SIZE;
     for(i = ARRAY_SIZE - 2; i >= 0; i--) {
         for(j = 0; j < ARRAY_SIZE; j++) {
-            idx = row_idx + j;
+            idx = row_offset + j;
             neighbour_idx = idx + ARRAY_SIZE;
             if(output_array[neighbour_idx] + 1 < output_array[idx])
                 output_array[idx] = output_array[neighbour_idx] + 1;
         }
-        row_idx -= ARRAY_SIZE;
+        row_offset -= ARRAY_SIZE;
     }
 
     // Iteration 5: Right to left
-    row_idx = 0;
+    row_offset = 0;
     for(i = 0; i < ARRAY_SIZE; i++) {
         for(j = ARRAY_SIZE - 2; j >= 0; j--) {
-            idx = row_idx + j;
+            idx = row_offset + j;
             neighbour_idx = idx + 1;
             if(output_array[neighbour_idx] + 1 < output_array[idx])
                 output_array[idx] = output_array[neighbour_idx] + 1;
         }
-        row_idx += ARRAY_SIZE;
+        row_offset += ARRAY_SIZE;
     }
 }
